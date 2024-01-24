8d9f8323b8e717069bd72700c5bc2a860e13dad9
erhic <ericngugi24@gmail.com>
2024-01-25 01:15:21 +0300

    feature: set navbar and component path using browserroutes tag

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from '../Pages/Navbar'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Createjob from "./Components/Createjob";
 import ApplyJob from "./Components/ApplyJob";
 import MyApplication from "./Components/MyApplication";
@@ -8,22 +8,27 @@ import Login from "./../Pages/Login";
 import Register from "./../Pages/Register";
 import Applicants from "./Components/Applicants";
 import Home from "./../Pages/Home";
-// import Private from "./../Pages/Private";
+import Private from "./../Pages/Private";
 
 export default function App() {
 
 
   return (
     <>
+
+
       <BrowserRouter>
-        <Home />
-        <Login />
-        <Register />
         <Navbar />
-        <MyApplication />
-        <Applicants />
-        <Createjob />
-        <ApplyJob />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/applyjobs' element={<ApplyJob />} />
+          <Route path='/myapplications' element={<MyApplication />} />
+          <Route path='/createjobs' element={<Createjob />} />
+          <Route path='/applicants' element={<Applicants />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+
+        </Routes>
       </BrowserRouter>
     </>
 
