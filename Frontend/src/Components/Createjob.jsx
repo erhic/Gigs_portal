31009be2e3da768e90ce5c57181340e58b2b42cf
erhic <ericngugi24@gmail.com>
2024-02-10 00:50:10 +0300

    feat: add a functionality to send data from form to the database

--- Frontend/src/Components/Createjob.jsx
+++ Frontend/src/Components/Createjob.jsx
@@ -1,25 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Createjob() {
+  const [jobInfo, setJobInfo] = useState({
+    companyName: '',
+    jobTitle: '',
+    companyLogo: '',
+    minimumSalary: '',
+    maximumSalary: '',
+    salaryType: '',
+    jobLocation: '',
+    experienceType: '',
+    employmentType: '',
+    jobDescription: ''
+
+
+  })
+  const handleChange = (e) => {
+    setJobInfo((prevData) => {
+      return { ...prevData, [e.target.name]: e.target.value }
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3501/createjobs', {
+      method: 'POST',
+      body: JSON.stringify(jobInfo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then((data) => {
+        console.log(data)
+        setJobInfo({
+          companyName: '',
+          jobTitle: '',
+          companyLogo: '',
+          minimumSalary: '',
+          maximumSalary: '',
+          salaryType: '',
+          jobLocation: '',
+          experienceType: '',
+          employmentType: '',
+          jobDescription: ''
+        }
+        )
+      })
+      .catch(err => console.log(err))
+
+  }
   return (
     <>
       <div className='row mx-auto'>
         <h3 className='text-center mb-4'> Create job</h3>
-        <form className='col-md-9 mx-auto'>
+        <form className='col-md-9 mx-auto' onSubmit={handleSubmit}>
 
 
           <div className="row">
             <div className="mb-3 col-md-4">
               <label className="form-label">Company Name</label>
-              <input type="text" className="form-control" />
+              <input type="text" required name='companyName' onChange={handleChange} className="form-control" />
             </div>
             <div className="mb-3 col-md-4">
               <label className="form-label">Job Title</label>
-              <input type="text" className="form-control" />
+              <input type="text" required name='jobTitle' onChange={handleChange} className="form-control" />
             </div>
             <div className="mb-3 col-md-4">
               <label className="form-label">Company Logo Link</label>
-              <input type="text" className="form-control" />
+              <input type="text" required name='companyLogo' onChange={handleChange} className="form-control" />
             </div>
           </div>
 
@@ -30,15 +78,15 @@ export default function Createjob() {
           <div className="row">
             <div className="mb-3 col-md-4">
               <label className="form-label">Minumum Salary</label>
-              <input type="number" className="form-control" />
+              <input type="number" required name='minimumSalary' onChange={handleChange} className="form-control" />
             </div>
             <div className="mb-3 col-md-4">
               <label className="form-label">Maximum Salary</label>
-              <input type="number" className="form-control" />
+              <input type="number" required name='maximumSalary' onChange={handleChange} className="form-control" />
             </div>
             <div className="mb-3 col-md-4">
               <label className="form-label">Salary Type</label>
-              <select className="form-select form-select-sm" >
+              <select required className="form-select form-select-sm" name='salaryType' onChange={handleChange}>
                 <option defaultValue=""></option>
                 <option value="Daily">Daily</option>
                 <option value="Monthly">Monthly</option>
@@ -52,16 +100,17 @@ export default function Createjob() {
           <div className="row">
             <div className="mb-3 col-md-4">
               <label className="form-label">Job Location</label>
-              <input type="Number" className="form-control" />
+              <input type="text" required name='jobLocation' onChange={handleChange} className="form-control" />
             </div>
 
             <div className="mb-3 col-md-4">
               <label className="form-label">Experience Level</label>
-              <input type="number" className="form-control" />
+              <input type="number" required name='experienceType' onChange={handleChange} className="form-control" />
             </div>
             <div className="mb-3 col-md-4">
               <label htmlFor="employmenttype" className="form-label">Employment Type</label>
-              <select defaultValue="" className="form-select form-select-sm" >
+              <select required name='employmentType' onChange={handleChange} className="form-select form-select-sm" >
+                <option defaultValue=""></option>
                 <option value="Daily">Part Time</option>
                 <option value="Monthly">Full Time</option>
                 <option value="Weekly">Internship</option>
@@ -74,7 +123,7 @@ export default function Createjob() {
 
 
           <label > Job Description</label><div className="form-floating mb-3">
-            <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
+            <textarea className="form-control" required name='jobDescription' onChange={handleChange} placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
 
           </div>
           <button type="submit" className="btn btn-primary text-secondary">Submit</button>
