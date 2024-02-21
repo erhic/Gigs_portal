9c512332a3c1c977d133399a5ae33b15e5034558
erhic <ericngugi24@gmail.com>
2024-02-21 23:56:27 +0300

    feat: guard routes

--- Backend/app.js
+++ Backend/app.js
@@ -75,7 +75,7 @@ app.post("/login", async (req, res) => {
 });
 
 //endpoint to post job
-app.post("/createjobs", async (req, res) => {
+app.post("/createjobs", verifyToken, async (req, res) => {
   let jobData = req.body;
   try {
     let jobs = await jobsModel.create(jobData);
@@ -99,7 +99,7 @@ app.get("/alljobs", async (req, res) => {
 });
 
 //endpoint to apply for jobs
-app.post("/applyjob", async (req, res) => {
+app.post("/applyjob", verifyToken, async (req, res) => {
   let applyData = req.body;
   try {
     let application = await applicationsModel.create(applyData);
@@ -123,7 +123,7 @@ app.get("/jobs/:jobid/:userid/", (req, res) => {
 });
 
 //endpoint to see applied jobs by recruiter
-app.get("/appliedjobs", async (req, res) => {
+app.get("/appliedjobs", verifyToken, async (req, res) => {
   try {
     let appliedJobs = await applicationsModel
       .find()
@@ -137,7 +137,7 @@ app.get("/appliedjobs", async (req, res) => {
 });
 
 //endpoint to give response to applicant by recruiter/ whether will proceed to next stage
-app.put("/appliedjobs/:id", (req, res) => {
+app.put("/appliedjobs/:id", verifyToken, (req, res) => {
   let applicantStatus = req.body;
   applicationsModel
     .updateOne({ _id: req.params.id }, applicantStatus)
